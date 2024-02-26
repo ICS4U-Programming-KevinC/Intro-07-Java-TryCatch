91bffcd40188448df59fda2823ad2484737bdb4d
Kevin Csiffary <kevin.csiffary@stu.ocsb.ca>
2024-02-26 14:59:23 +0000

    Added try catch

--- VolSphere.java
+++ VolSphere.java
@@ -19,27 +19,33 @@ public final class VolSphere {
    * @param args Command line arguments.
    */
   public static void main(final String[] args) {
+    // Setup try catch for non number error.
+    try {
+      // Create the scanner.
+      Scanner sc = new Scanner(System.in);
 
-    // Create the scanner.
-    Scanner sc = new Scanner(System.in);
+      // Ask user for their radius.
+      System.out.println("This program calculates the volume of a sphere!!");
+      System.out.println("What is the radius of your sphere?");
+      String radString = sc.nextLine();
 
-    // Ask user for their radius.
-    System.out.println("This program calculates the volume of a sphere!!");
-    System.out.println("What is the radius of your sphere?");
-    String radString = sc.nextLine();
+      // Cast the input to a double.
+      double radDouble = Double.parseDouble(radString);
 
-    // Cast the input to a double.
-    double radDouble = Double.parseDouble(radString);
+      if (radDouble < 0) {
+        // Tell user they cant have a negative number.
+        System.out.println("You can't have a negative radius!");
+      } else {
+        // Calculate volume.
+        double volume = ((4 / 3.0) * Math.PI * Math.pow(radDouble, 3));
 
-    if (radDouble < 0) {
-      // Tell user they cant have a negative number.
-      System.out.println("You can't have a negative radius!");
-    } else {
-      // Calculate volume.
-      double volume = ((4 / 3.0) * Math.PI * Math.pow(radDouble, 3));
-
-      // Display the volume to the user.
-      System.out.println("The volume of your sphere is: " + volume);
+        // Display the volume to the user.
+        System.out.println("The volume of your sphere is: " + volume);
+      }
+    } catch (Exception e) {
+      // Handles non number error
+      System.out.print("There was an error, ");
+      System.out.println("ensure you made a correct numerical input!");
     }
   }
 }
